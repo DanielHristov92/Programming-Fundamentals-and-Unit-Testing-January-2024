030f60be1b83db3f7f55410311365e93f877486d
DanielHristov92 <157898445+DanielHristov92@users.noreply.github.com>
2024-01-27 18:40:07 +0200

    Add files via upload

---
+++ Complex Conditional Statements/09.Cinema/Program.cs
@@ -0,0 +1,25 @@
+﻿string type = Console.ReadLine();
+int count = int.Parse(Console.ReadLine());
+int seats = int.Parse(Console.ReadLine());
+
+if (type == "Premiere")
+{
+    double totalAmount = count * seats;
+    double premiere = 12.00;
+    double totalCost = totalAmount * premiere;
+    Console.WriteLine($"{totalCost:F2}");
+}
+if (type == "Normal")
+{
+    double totalAmount = count * seats;
+    double normal = 7.50;
+    double totalCost = totalAmount * normal;
+    Console.WriteLine($"{totalCost:F2}");
+}
+if (type == "Discount")
+{
+    double totalAmount = count * seats;
+    double discount = 5.00;
+    double totalCost = totalAmount * discount;
+    Console.WriteLine($"{totalCost:F2}");
+}
