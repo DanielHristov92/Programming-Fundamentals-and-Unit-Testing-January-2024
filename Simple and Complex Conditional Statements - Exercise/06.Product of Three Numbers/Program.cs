df52bd6cad9a4fa4c9ca2ca85450e44ab07f2e69
DanielHristov92 <157898445+DanielHristov92@users.noreply.github.com>
2024-01-27 18:49:17 +0200

    Add files via upload

---
+++ Simple and Complex Conditional Statements - Exercise/06.Product of Three Numbers/Program.cs
@@ -0,0 +1,18 @@
+﻿int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+int num3 = int.Parse(Console.ReadLine());
+
+int operation = num1 * num2 * num3;
+
+if (operation > 0)
+{
+    Console.WriteLine("positive");
+}
+else if (operation < 0)
+{
+    Console.WriteLine("negative");
+}
+else
+{
+    Console.WriteLine("zero");
+}
---
+++ Simple and Complex Conditional Statements - Exercise/09.Sum Seconds/Program.cs
@@ -0,0 +1,10 @@
+﻿int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+int num3 = int.Parse(Console.ReadLine());
+
+int total = num1 + num2 + num3;
+
+int timeMinutes = total / 60;
+int timeSeconds = total % 60;
+
+Console.WriteLine($"{timeMinutes}:{timeSeconds:D2}");
