1c2f7b44d01ee158d77c4697c7c517267824b570
DanielHristov92 <157898445+DanielHristov92@users.noreply.github.com>
2024-01-27 17:53:59 +0200

    Add files via upload

---
+++ Simple Commands and Visual Studio - Exercise/05.Triangle Perimeter/Program.cs
@@ -0,0 +1,6 @@
+﻿int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+
+int perimeter = a + b + c;
+Console.WriteLine(perimeter);
---
+++ Simple Commands and Visual Studio - Exercise/06.Inches to Centimeters Converter/Program.cs
@@ -0,0 +1,4 @@
+﻿int inches = int.Parse(Console.ReadLine());
+
+double convert = inches * 2.54;
+Console.WriteLine(convert);
