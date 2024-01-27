7f7a59c88c536357d15ae4a36f9f86ccfb9e5726
DanielHristov92 <157898445+DanielHristov92@users.noreply.github.com>
2024-01-27 18:48:12 +0200

    Add files via upload

---
+++ Simple and Complex Conditional Statements - Exercise/04.Number Operations/Program.cs
@@ -0,0 +1,24 @@
+﻿double number1 = double.Parse(Console.ReadLine());
+double number2 = double.Parse(Console.ReadLine());
+string mathOperator = Console.ReadLine();
+
+if (mathOperator == "+")
+{
+    double sum = number1 + number2;
+    Console.WriteLine($"{number1} {mathOperator} {number2} = {sum:F2}");
+}
+else if (mathOperator == "-")
+{
+    double minus = number1 - number2;
+    Console.WriteLine($"{number1} {mathOperator} {number2} = {minus:F2}");
+}
+else if (mathOperator == "*")
+{
+    double plus = number1 * number2;
+    Console.WriteLine($"{number1} {mathOperator} {number2} = {plus:F2}");
+}
+else if (mathOperator == "/")
+{
+    double asc = number1 / number2;
+    Console.WriteLine($"{number1} {mathOperator} {number2} = {asc:F2}");
+}
---
+++ Simple and Complex Conditional Statements - Exercise/05.Vacation Expenses/Program.cs
@@ -0,0 +1,63 @@
+﻿string season = Console.ReadLine();
+string type = Console.ReadLine();
+int days = int.Parse(Console.ReadLine());
+
+double discount = 0;
+double pricePerNight = 0;
+
+if (season == "Spring")
+{
+    discount = 0.2;
+    if (type == "Hotel")
+    {
+        pricePerNight = 30;
+    }
+    else if (type == "Camping")
+    {
+        pricePerNight = 10;
+    }
+
+}
+else if (season == "Summer")
+{
+    discount = 0;
+    if (type == "Hotel")
+    {
+        pricePerNight = 50;
+    }
+    else if (type == "Camping")
+    {
+        pricePerNight = 30;
+    }
+
+}
+else if (season == "Autumn")
+{
+    discount = 0.3;
+    if (type == "Hotel")
+    {
+        pricePerNight = 20;
+    }
+    else if (type == "Camping")
+    {
+        pricePerNight = 15;
+    }
+
+}
+if (season == "Winter")
+{
+    discount = 0.1;
+    if (type == "Hotel")
+    {
+        pricePerNight = 40;
+    }
+    else if (type == "Camping")
+    {
+        pricePerNight = 10;
+    }
+
+}
+double totalPriceNoDiscount = pricePerNight * days;
+double totalPrice = totalPriceNoDiscount - (totalPriceNoDiscount * discount);
+
+Console.WriteLine($"{totalPrice:F2}");
