f9629847cf04190f1736ea0ae80247577d67c4e4
DanielHristov92 <157898445+DanielHristov92@users.noreply.github.com>
2024-01-27 18:39:10 +0200

    Add files via upload

---
+++ Complex Conditional Statements/03.Bonus Score/Program.cs
@@ -0,0 +1,15 @@
+﻿int point = int.Parse(Console.ReadLine());
+
+if (point >= 0 && point <= 3)
+{
+    point = point + 5;
+}
+else if (point >= 4 && point <= 6)
+{
+    point = point + 15;
+}
+else if (point >= 7 && point <= 9)
+{
+    point = point + 20;
+}
+Console.WriteLine(point);
---
+++ Complex Conditional Statements/04.Food or Drink/Program.cs
@@ -0,0 +1,42 @@
+﻿string product = Console.ReadLine();
+
+if (product == "curry")
+{
+    Console.WriteLine("food");
+}
+else if (product == "noodles")
+{
+    Console.WriteLine("food");
+}
+else if (product == "sushi")
+{
+    Console.WriteLine("food");
+}
+else if (product == "spaghetti")
+{
+    Console.WriteLine("food");
+}
+else if (product == "bread")
+{
+    Console.WriteLine("food");
+}
+else if (product == "tea")
+{
+    Console.WriteLine("drink");
+}
+else if (product == "water")
+{
+    Console.WriteLine("drink");
+}
+else if (product == "coffee")
+{
+    Console.WriteLine("drink");
+}
+else if (product == "juice")
+{
+    Console.WriteLine("drink");
+}
+else
+{
+    Console.WriteLine("unknown");
+}
