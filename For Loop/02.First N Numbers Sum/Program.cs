b98452776698826172885171940ac64e453527d2
DanielHristov92 <157898445+DanielHristov92@users.noreply.github.com>
2024-01-29 21:20:37 +0200

    Add files via upload

---
+++ For Loop/02.First N Numbers Sum/Program.cs
@@ -0,0 +1,10 @@
+﻿int num = int.Parse(Console.ReadLine());
+int sum = 0;
+
+for (int i = 1; i < num; i++)
+{
+    sum += i;
+    Console.Write($"{i}+");
+}
+sum += num;
+Console.WriteLine($"{num}={sum}");
