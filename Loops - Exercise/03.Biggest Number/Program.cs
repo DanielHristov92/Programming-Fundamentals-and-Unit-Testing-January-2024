c719e0644eba27d15d8731303cfd10893b0ff950
DanielHristov92 <157898445+DanielHristov92@users.noreply.github.com>
2024-02-04 08:27:14 +0200

    Add files via upload

---
+++ Loops - Exercise/03.Biggest Number/Program.cs
@@ -0,0 +1,13 @@
+﻿int num =  int.Parse(Console.ReadLine());
+int max = 0;
+
+for (int i = 0; i < num; i++)
+{
+ int current = int.Parse(Console.ReadLine());
+
+    if (current > max)
+    {
+     max = current;
+    }
+}
+Console.WriteLine(max);
