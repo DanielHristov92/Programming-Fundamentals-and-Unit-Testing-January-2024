bc8d440e7d237afe4017f98740e4506af96f2cf9
DanielHristov92 <157898445+DanielHristov92@users.noreply.github.com>
2024-02-04 08:29:25 +0200

    Add files via upload

---
+++ Loops - Exercise/07.Special Bonus/Program.cs
@@ -0,0 +1,14 @@
+﻿int stopNumber = int.Parse(Console.ReadLine());
+
+int previousNumber = 0;
+
+while (true)
+{
+    int number = int.Parse(Console.ReadLine());
+    if (number == stopNumber)
+    {
+        Console.WriteLine(previousNumber * 1.2);
+        break;
+    }
+    previousNumber = number;
+}
