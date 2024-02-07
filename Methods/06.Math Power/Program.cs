9e5fa33ea6790808142b0aeca30b52ae2a932a75
DanielHristov92 <157898445+DanielHristov92@users.noreply.github.com>
2024-02-07 21:53:56 +0200

    Add files via upload

---
+++ Methods/06.Math Power/Program.cs
@@ -0,0 +1,13 @@
+﻿int baseNum = int.Parse(Console.ReadLine());
+int p = int.Parse(Console.ReadLine());
+
+Console.WriteLine(MathPower(baseNum, p));
+static int MathPower(int baseNum, int power)
+{
+    int output = baseNum;
+    for (int i = 2; i <= power; i++)
+    {
+        output *= baseNum;
+    }
+    return output;
+}
