2359e7fc30d41d9b92b22cbcf2bf4c45d5135310
DanielHristov92 <157898445+DanielHristov92@users.noreply.github.com>
2024-02-08 20:44:15 +0200

    Add files via upload

---
+++ Nested Loops and Methods - Exercise/07.Factorial Division/Program.cs
@@ -0,0 +1,18 @@
+﻿int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+
+int factorial1 = CalculateFactorial(num1);
+int factorial2 = CalculateFactorial(num2);
+
+int result = factorial1 / factorial2;
+
+Console.WriteLine(result);
+ static int CalculateFactorial(int n)
+{
+    int factorial = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        factorial *= i;
+    }
+    return factorial;
+}
---
+++ Nested Loops and Methods - Exercise/08.Multiplication Sign/Program.cs
@@ -0,0 +1,33 @@
+﻿int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+int num3 = int.Parse(Console.ReadLine());
+
+string result = GetProductSign(num1, num2, num3);
+
+Console.WriteLine(result);
+    static string GetProductSign(int a, int b, int c)
+{
+    int countNegative = 0;
+
+    if (a == 0 || b == 0 || c == 0)
+    {
+        return "zero";
+    }
+
+    if (a < 0)
+    {
+        countNegative++;
+    }
+
+    if (b < 0)
+    {
+        countNegative++;
+    }
+
+    if (c < 0)
+    {
+        countNegative++;
+    }
+
+    return countNegative % 2 == 0 ? "positive" : "negative";
+}
