0db3dafab4d2433d0b9f1707a0ed50dc6c287705
DanielHristov92 <157898445+DanielHristov92@users.noreply.github.com>
2024-02-08 20:14:16 +0200

    Add files via upload

---
+++ Nested Loops and Methods - Exercise/01.Stupid Passwords/Program.cs
@@ -0,0 +1,11 @@
+﻿int N = int.Parse(Console.ReadLine());
+
+for (int i = 2; i <= N; i += 2)
+{
+    for (int j = 1; j <= N; j += 2)
+    {
+        int product = i * j;
+        Console.Write($"{i}{j}{product} ");
+    }
+}
+   
---
+++ Nested Loops and Methods - Exercise/02.Magic Numbers/Program.cs
@@ -0,0 +1,15 @@
+﻿int N = int.Parse(Console.ReadLine());
+
+for (int a = 0; a <= 9; a++)
+{
+    for (int b = 0; b <= 9; b++)
+    {
+        for (int c = 0; c <= 9; c++)
+        {
+            if (a * b * c == N)
+            {
+                Console.Write($" {a}{b}{c}");
+            }
+        }
+    }
+}
