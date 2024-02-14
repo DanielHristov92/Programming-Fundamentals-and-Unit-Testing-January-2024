a4b1c4d8858f38c8caf25a8f79d9d4622cbca70f
DanielHristov92 <157898445+DanielHristov92@users.noreply.github.com>
2024-02-14 21:02:33 +0200

    Add files via upload

---
+++ Lists/04.List Manipulation Basics/Program.cs
@@ -0,0 +1,34 @@
+﻿List<int> numbers = Console.ReadLine()
+                                    .Split()
+                                    .Select(int.Parse)
+                                    .ToList();
+
+string command = Console.ReadLine();
+while (command != "end")
+{
+    string[] tokens = command.Split();
+    string action = tokens[0];
+    int number = int.Parse(tokens[1]);
+
+    switch (action)
+    {
+        case "Add":
+            numbers.Add(number);
+            break;
+        case "Remove":
+            numbers.Remove(number);
+            break;
+        case "RemoveAt":
+            int indexToRemove = number;
+            numbers.RemoveAt(indexToRemove);
+            break;
+        case "Insert":
+            int indexToInsert = int.Parse(tokens[2]);
+            numbers.Insert(indexToInsert, number);
+            break;
+    }
+
+    command = Console.ReadLine();
+}
+
+Console.WriteLine(string.Join(" ", numbers));
---
+++ Lists/05.List Manipulation Advanced/Program.cs
@@ -0,0 +1,60 @@
+﻿List<int> numbers = Console.ReadLine()
+                                    .Split()
+                                    .Select(int.Parse)
+                                    .ToList();
+
+string command = Console.ReadLine();
+while (command != "end")
+{
+    string[] tokens = command.Split();
+    string action = tokens[0];
+
+    switch (action)
+    {
+        case "Contains":
+            int numberToCheck = int.Parse(tokens[1]);
+            if (numbers.Contains(numberToCheck))
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No such number");
+            }
+            break;
+        case "PrintEven":
+            Console.WriteLine(string.Join(" ", numbers.Where(n => n % 2 == 0)));
+            break;
+        case "PrintOdd":
+            Console.WriteLine(string.Join(" ", numbers.Where(n => n % 2 != 0)));
+            break;
+        case "GetSum":
+            Console.WriteLine(numbers.Sum());
+            break;
+        case "Filter":
+            string condition = tokens[1];
+            int filterNumber = int.Parse(tokens[2]);
+            numbers = FilterList(numbers, condition, filterNumber);
+            break;
+    }
+
+    command = Console.ReadLine();
+}
+
+Console.WriteLine(string.Join(" ", numbers));
+    static List<int> FilterList(List<int> numbers, string condition, int filterNumber)
+{
+    switch (condition)
+    {
+        case "<":
+            return numbers.Where(n => n < filterNumber).ToList();
+        case ">":
+            return numbers.Where(n => n > filterNumber).ToList();
+        case "<=":
+            return numbers.Where(n => n <= filterNumber).ToList();
+        case ">=":
+            return numbers.Where(n => n >= filterNumber).ToList();
+        default:
+            return numbers;
+    }
+}
