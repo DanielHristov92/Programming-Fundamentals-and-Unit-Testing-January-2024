87d3651528ca63b9339d0f59ec6fd35ece637a74
DanielHristov92 <157898445+DanielHristov92@users.noreply.github.com>
2024-02-15 20:57:00 +0200

    Add files via upload

---
+++ Arrays and Lists - Exercise/01.Gauss' Trick/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+int length = numbers.Length;
+
+for (int i = 0; i < (length + 1) / 2; i++)
+{
+    int sum = numbers[i] + (i != length - 1 - i ? numbers[length - 1 - i] : 0);
+
+    Console.Write(sum + " ");
+}
+
+Console.WriteLine();
---
+++ Arrays and Lists - Exercise/03.Merging Lists/Program.cs
@@ -0,0 +1,24 @@
+﻿List<int> firstSequence = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+List<int> secondSequence = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+List<int> result = new List<int>();
+
+int minLength = Math.Min(firstSequence.Count, secondSequence.Count);
+
+for (int i = 0; i < minLength; i++)
+{
+    result.Add(firstSequence[i]);
+    result.Add(secondSequence[i]);
+}
+
+if (firstSequence.Count > minLength)
+{
+    result.AddRange(firstSequence.GetRange(minLength, firstSequence.Count - minLength));
+}
+else if (secondSequence.Count > minLength)
+{
+    result.AddRange(secondSequence.GetRange(minLength, secondSequence.Count - minLength));
+}
+
+Console.WriteLine(string.Join(" ", result));
