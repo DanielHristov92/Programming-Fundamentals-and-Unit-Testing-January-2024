a4ddcdf7b9565576b763b0106d665fd16f715673
DanielHristov92 <157898445+DanielHristov92@users.noreply.github.com>
2024-02-26 20:51:09 +0200

    Add files via upload

---
+++ Exam Preparation/01. Sum Factorial Even Digits/Program.cs
@@ -0,0 +1,33 @@
+﻿int number = int.Parse(Console.ReadLine());
+
+Console.WriteLine(SumOfEvenDigitFactorials(number));
+
+    static long SumOfEvenDigitFactorials(int number)
+{
+    long sum = 0;
+
+    while (number > 0)
+    {
+        int digit = number % 10;
+        number /= 10;
+
+        if (digit % 2 == 0)
+        {
+            long factorial = CalculateFactorial(digit);
+
+            sum += factorial;
+        }
+    }
+
+    return sum;
+}
+
+static long CalculateFactorial(int n)
+{
+    long factorial = 1;
+    for (int i = 2; i <= n; i++)
+    {
+        factorial *= i;
+    }
+    return factorial;
+}
---
+++ Exam Preparation/02.Middle Elements/Program.cs
@@ -0,0 +1,19 @@
+﻿string input = Console.ReadLine();
+string[] numbers = input.Split(' ');
+
+int[] array = new int[numbers.Length];
+for (int i = 0; i < numbers.Length; i++)
+{
+    array[i] = int.Parse(numbers[i]);
+}
+
+int middleStart = array.Length / 2 - 1;
+int middleEnd = array.Length / 2;
+int sum = 0;
+for (int i = middleStart; i <= middleEnd; i++)
+{
+    sum += array[i];
+}
+
+double average = (double)sum / (middleEnd - middleStart + 1);
+Console.WriteLine($"{average:F2}");
