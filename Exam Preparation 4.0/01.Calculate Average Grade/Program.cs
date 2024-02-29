edc8029e739a2e805953b01392e880304878db1f
DanielHristov92 <157898445+DanielHristov92@users.noreply.github.com>
2024-02-29 18:57:08 +0200

    Add files via upload

---
+++ Exam Preparation 4.0/01.Calculate Average Grade/Program.cs
@@ -0,0 +1,11 @@
+﻿int N = int.Parse(Console.ReadLine());
+double sum = 0;
+
+for (int i = 0; i < N; i++)
+{
+    double grade = double.Parse(Console.ReadLine());
+    sum += grade;
+}
+
+double averageGrade = sum / N;
+Console.WriteLine($"{averageGrade:F2}");
---
+++ Exam Preparation 4.0/03.Bigger Numbers/Program.cs
@@ -0,0 +1,12 @@
+﻿string[] numbersInput = Console.ReadLine().Split(' ');
+
+int N = int.Parse(Console.ReadLine());
+
+foreach (string numberStr in numbersInput)
+{
+    int number = int.Parse(numberStr);
+    if (number > N)
+    {
+        Console.Write(number + " ");
+    }
+}
