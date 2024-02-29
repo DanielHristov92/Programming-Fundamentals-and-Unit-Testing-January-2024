d7a0fbb639bc8455ae6ca7982cdd383107de8d91
DanielHristov92 <157898445+DanielHristov92@users.noreply.github.com>
2024-02-29 17:27:45 +0200

    Add files via upload

---
+++ Exam Preparation 2.0/01.Magic Numbers/Program.cs
@@ -0,0 +1,60 @@
+﻿int N = int.Parse(Console.ReadLine());
+bool foundMagicNumber = false;
+
+for (int number = 1; number <= N; number++)
+{
+    if (IsMagicNumber(number))
+    {
+        Console.Write(number + " ");
+        foundMagicNumber = true;
+    }
+}
+
+if (!foundMagicNumber)
+{
+    Console.WriteLine("no");
+}
+    static bool IsMagicNumber(int number)
+{
+    return IsAllDigitsPrime(number) && SumOfDigits(number) % 2 == 0;
+}
+
+static bool IsAllDigitsPrime(int number)
+{
+    while (number > 0)
+    {
+        int digit = number % 10;
+        if (!IsPrime(digit))
+        {
+            return false;
+        }
+        number /= 10;
+    }
+    return true;
+}
+
+static int SumOfDigits(int number)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        sum += number % 10;
+        number /= 10;
+    }
+    return sum;
+}
+static bool IsPrime(int number)
+{
+    if (number <= 1)
+    {
+        return false;
+    }
+    for (int i = 2; i <= Math.Sqrt(number); i++)
+    {
+        if (number % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
---
+++ Exam Preparation 2.0/02.Min&Max Values/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] numbers = Console.ReadLine()
+                        .Split(' ')
+                        .Select(int.Parse)
+                        .ToArray();
+
+int num = int.Parse(Console.ReadLine());
+
+int max = numbers.Take(num).Max();
+int min = numbers.Take(num).Min();
+
+Console.WriteLine(max);
+Console.WriteLine(min);
