d201143724a68397b7a71bb4c1b0e624f02fa4d5
DanielHristov92 <157898445+DanielHristov92@users.noreply.github.com>
2024-03-01 15:15:17 +0200

    Add files via upload

---
+++ Exam Preparation 3.0/01.English Name of Each Digit/Program.cs
@@ -0,0 +1,35 @@
+﻿int number = int.Parse(Console.ReadLine());
+
+while (number > 0)
+{
+    int digit = number % 10;
+    string digitName = GetDigitName(digit);
+    Console.WriteLine(digitName);
+    number /= 10;
+}
+        static string GetDigitName(int digit)
+{
+    switch (digit)
+    {
+        case 1:
+            return "one";
+        case 2:
+            return "two";
+        case 3:
+            return "three";
+        case 4:
+            return "four";
+        case 5:
+            return "five";
+        case 6:
+            return "six";
+        case 7:
+            return "seven";
+        case 8:
+            return "eight";
+        case 9:
+            return "nine";
+        default:
+            return "";
+    }
+}
---
+++ Exam Preparation 3.0/02.Average Last Elements/Program.cs
@@ -0,0 +1,17 @@
+﻿string[] inputArray = Console.ReadLine().Split();
+int[] numbers = Array.ConvertAll(inputArray, int.Parse);
+
+int num = int.Parse(Console.ReadLine());
+
+int sum = 0;
+for (int i = numbers.Length - num; i < numbers.Length; i++)
+{
+    sum += numbers[i];
+}
+
+double average = (double)sum / num;
+
+Console.WriteLine($"{average:F2}");
+
+
+
