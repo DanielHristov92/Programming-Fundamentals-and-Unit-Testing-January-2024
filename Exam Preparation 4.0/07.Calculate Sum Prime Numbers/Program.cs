cb8fa3540ae6b7c5744e7cb189650a4659aca176
DanielHristov92 <157898445+DanielHristov92@users.noreply.github.com>
2024-03-01 15:59:29 +0200

    Add files via upload

---
+++ Exam Preparation 4.0/07.Calculate Sum Prime Numbers/Program.cs
@@ -0,0 +1,42 @@
+﻿int N = int.Parse(Console.ReadLine());
+
+List<int> numbers = new List<int>();
+
+for (int i = 0; i < N; i++)
+{
+    int num = int.Parse(Console.ReadLine());
+    numbers.Add(num);
+}
+
+int sum = SumOfPrimes(numbers);
+Console.WriteLine(sum);
+
+        static bool IsPrime(int num)
+{
+    if (num <= 1)
+    {
+        return false;
+    }
+
+    for (int i = 2; i * i <= num; i++)
+    {
+        if (num % i == 0)
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+static int SumOfPrimes(List<int> numbers)
+{
+    int sum = 0;
+    foreach (int num in numbers)
+    {
+        if (IsPrime(num))
+        {
+            sum += num;
+        }
+    }
+    return sum;
+}
